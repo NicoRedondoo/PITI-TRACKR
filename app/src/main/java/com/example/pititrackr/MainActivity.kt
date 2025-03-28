60000d92c89c8190dd00cee77603dfa419d9bc53
Nico <nicredmor@alum.us.es>
2025-03-28 19:36:09 +0100

    upbar y button bien

--- app/src/main/java/com/example/pititrackr/MainActivity.kt
+++ app/src/main/java/com/example/pititrackr/MainActivity.kt
@@ -1,16 +1,20 @@
+package com.example.pititrackr
 import android.content.Context
 import android.os.Bundle
-import android.widget.Button
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pititrackr.R
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var counterTextView: TextView
-    private lateinit var smokeButton: Button
+    private lateinit var smokeButton: ImageButton
     private val prefs by lazy { getSharedPreferences("SmokePrefs", Context.MODE_PRIVATE) }
+    private lateinit var touchAnimation: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +23,12 @@ class MainActivity : AppCompatActivity() {
         counterTextView = findViewById(R.id.counterTextView)
         smokeButton = findViewById(R.id.smokeButton)
 
+        touchAnimation = AnimationUtils.loadAnimation(this, R.anim.touch_button)
+
         updateCounter()
 
         smokeButton.setOnClickListener {
+            smokeButton.startAnimation(touchAnimation)
             val today = getCurrentDate()
             val count = prefs.getInt(today, 0) + 1
             prefs.edit().putInt(today, count).apply()
@@ -32,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     private fun updateCounter() {
         val today = getCurrentDate()
         val count = prefs.getInt(today, 0)
-        counterTextView.text = "Cigarros fumados hoy: $count"
+        counterTextView.text = "$count"
     }
 
     private fun getCurrentDate(): String {
